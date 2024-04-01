d4c32142dae6b4714b370f81b2e5972171e125e8
ZeyuCui <64547984+ZeyuCui@users.noreply.github.com>
2024-04-01 13:12:16 -0700

    initialize spring framework

---
+++ crypto/src/main/java/com/gateZ/crypto/CryptoApplication.java
@@ -0,0 +1,13 @@
+package com.gateZ.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoApplication.class, args);
+	}
+
+}
